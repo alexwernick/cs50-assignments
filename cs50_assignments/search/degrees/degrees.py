88b22a7187cd84eaaed7598f74f22ee03f5e12fb
Alex Wernick <alex.j.wernick@gmail.com>
2024-08-12 17:58:16 +0100

    black

--- cs50_assignments/search/degrees/degrees.py
+++ cs50_assignments/search/degrees/degrees.py
@@ -1,6 +1,8 @@
 import csv
 import sys
 
+from cs50_assignments.search.degrees.util import Node, QueueFrontier
+
 # Maps names to a set of corresponding person_ids
 names = {}
 
@@ -90,8 +92,40 @@ def shortest_path(source, target):
     If no possible path, returns None.
     """
 
-    # TODO
-    raise NotImplementedError
+    # Initialize frontier to just the starting position
+    start = Node(state=source, parent=None, action=None)
+    frontier = QueueFrontier()
+    frontier.add(start)
+
+    # Initialize an empty explored set
+    explored = set()
+
+    # Keep looping until solution found
+    while True:
+        # If nothing left in frontier, then no path
+        if frontier.empty():
+            return None
+
+        # Choose a node from the frontier
+        node = frontier.remove()
+
+        # If node is the goal, then we have a solution
+        if node.state == target:
+            relationships = []
+            while node.parent is not None:
+                relationships.append((node.action, node.state))
+                node = node.parent
+            relationships.reverse()
+            return relationships
+
+        # Mark node as explored
+        explored.add(node.state)
+
+        # Add neighbors to frontier
+        for action, state in neighbors_for_person(node.state):
+            if not frontier.contains_state(state) and state not in explored:
+                child = Node(state=state, parent=node, action=action)
+                frontier.add(child)
 
 
 def person_id_for_name(name):
